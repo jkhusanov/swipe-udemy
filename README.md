dcf04a368bbf33b30da23499b6dd7ed5b1701cd5
Jakhongir Khusanov <worldj95@gmail.com>
2018-08-10 21:50:25 -0700

    Add animation to all cards and implement remove each of them after a swipe to right or left

--- README.md
+++ README.md
@@ -28,12 +28,11 @@ exp ios
 - [x] Start with simple ball animation to get started with **Animated** library
   - [x] Animate the ball
 - [x] Add **Cards** and animate one of them
+- [x] Add animation to all Cards and **Remove** each of them after a swipe to right/left
 - [ ] Attach screenshots/gif of screens to `README.MD`
 
 ### Wireframes
 <div style={{display: flex; flex-direction: row}}>
-  <b>Step 1</b>
-  <img  alt="wireframe1" src="screenshots/wireframe1.png" width="500" />
-  <b>Step 2</b>
-  <img  alt="wireframe2" src="screenshots/wireframe2.png" width="500" />
+  <img  alt="wireframe1" src="screenshots/wireframe1.png" width="450" />
+  <img  alt="wireframe2" src="screenshots/wireframe2.png" width="450" />
 </div>
--- app/Deck.js
+++ app/Deck.js
@@ -2,10 +2,21 @@ import React from 'react';
 import {
   View,
   Animated,
-  PanResponder
+  PanResponder,
+  Dimensions,
 } from 'react-native';
 
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
 export default class Deck extends React.Component {
+  // default props when reusable components are used, so we don't have to check each time
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {},
+  }
   constructor(props) {
     super(props)
 
@@ -15,22 +26,65 @@ export default class Deck extends React.Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       }, //called anytime user starts to move fingers on the screen 
-      onPanResponderRelease: () => { } //called when user removes finger from the screen
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right');
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left');
+        } else {
+          this.resetPosition();
+        }
+      } //called when user removes finger from the screen
     });
     this._panResponder = { panResponder, position }
+    this.state = { index: 0 }
+  }
+
+
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this._panResponder.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index]
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this._panResponder.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1});
+
+  }
+
+  resetPosition() {
+    Animated.spring(this._panResponder.position, {
+      toValue: { x: 0, y: 0 }
+    }).start()
   }
 
   getCardStyle() {
+    const { position } = this._panResponder
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
     return {
-      ...this._panResponder.position.getLayout(),
-      transform: [{ rotate: '45deg'}]
+      ...position.getLayout(),
+      transform: [{ rotate }]
     }
-    
+
   }
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) { return null; }
+
+      if (i === this.state.index) {
         return (
           <Animated.View
             key={item.id}
